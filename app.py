653ba218159b6f371f6ce3966b4d8845fb898a62
castroinvest <diego@castroinvest.com>
2023-01-03 13:32:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
+@app.route('/', subdomain='twitter')
+def twitter_redirect():
+    return redirect('https://twitter.com/ZeroUmBlog')
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/', subdomain='medium')
+def medium_redirect():
+    return redirect('https://medium.com/@zeroumblog')
+
+@app.route('/', subdomain='youtube')
+def youtube_redirect():
+    return redirect('https://www.youtube.com/zeroumblog')
+
+@app.route('/', subdomain='zeroum')
+def zeroum_redirect():
+    return redirect('https://zeroum.blog/index.html')
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
